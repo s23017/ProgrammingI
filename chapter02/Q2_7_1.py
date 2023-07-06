68879f54c3b60bcebeb5dc2bb7778b12a4139982
Toma Koshiro <s23017@std.it-college.ac.jp>
2023-07-06 07:40:12 +0000

    Q2_7

---
+++ chapter02/Q2_7_1.py
@@ -0,0 +1,6 @@
+a = []
+a.append("orange")
+a.append("apple")
+a.append("grape")
+a.append("banana")
+print(a)
---
+++ chapter02/Q2_7_2.py
@@ -0,0 +1,5 @@
+list = ["orange", "apple", "grape", "banana"]
+u = sorted(list)
+d = sorted(list, reverse=True)
+print(u)
+print(d)
