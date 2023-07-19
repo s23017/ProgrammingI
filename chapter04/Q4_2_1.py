74029177bb2d21e7b3dbb7b40b0a0998223fe625
Toma Koshiro <s23017@std.it-college.ac.jp>
2023-07-19 07:28:21 +0000

    Q4_1,2push

---
+++ chapter04/Q4_2_1.py
@@ -0,0 +1,13 @@
+def number_to_day(num=0):
+    if num == 0:
+        day = "今日"
+    elif num == 1:
+        day = "明日"
+    elif num == -1:
+        day = "昨日"
+    else:
+        day = "今日より1日を超えて離れた日"
+    return day
+
+
+print(number_to_day(1))
---
+++ chapter04/Q4_2_2.py
@@ -0,0 +1,10 @@
+def perrin(m=100):
+    a, b, c = 3, 0, 2
+    result = []
+    while a < m:
+        result.append(a)
+        a, b, c = b, c, a + b
+    return result
+
+
+print(perrin())
